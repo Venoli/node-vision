28832c8406156b5f0623f5dc6f646698ec33088d
venoli <venolishehara@gmail.com>
2022-03-30 14:55:56 +0530

    Code refined

--- routes/vision.js
+++ routes/vision.js
@@ -3,10 +3,16 @@ var router = express.Router();
 var AWS = require('aws-sdk');
 require('dotenv').config({ debug: true })
 
+
 router.post('/classify', function(req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
   let response = [];
   // Your code starts here //
+
+  /***************************************************************
+  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+  *****************************************************************/
 
   // AWS Configurations
   AWS.config.update({
@@ -15,37 +21,43 @@ router.post('/classify', function(req, res, next) {
     secretAccessKey: process.env.AWS_SECRET_KEY,
     })
 
-  // Calling Rekognition API
+  // Rekognition API
   const client = new AWS.Rekognition();
+
+  //Read Image Data
   var imageData = Buffer.from(req.files.file.data, 'base64');
-  
-    const params = {
+
+  const params = {
       Image: {
             Bytes: imageData
           }
     }
     
-    client.detectLabels(params, function(err, awsResponse) {
+  //Calling Rekognition API 
+  client.detectLabels(params, function(err, awsResponse) {
      if (err) {
-      console.log(err, err.headers); // if an error occurred
+        console.log('Error: ',err);
+        HandleErrors(err)
 
-      HandleErrors(err)
-
-    } else {
-     
-      console.log('d1',response)
-
-      response = awsResponse.Labels.map((label) => label.Name)
-      console.log('d2',response)
-      GettLabels(response)
-
-    } 
+      } else {
+        response = awsResponse.Labels.map((label) => label.Name)
+        console.log('Response: ',response)
+        GettLabels(response)
+      } 
     });
 
-    const HandleErrors = (err) => {
-       res.status(500).json({ "error":"Unable to process the request" });
-    }
 
+  // Error Handling
+  const HandleErrors = (err) => {
+    /**************************************************
+    Code adapted from ExpressJs Documentation example:
+    https://expressjs.com/en/api.html#res
+    **************************************************/
+       res.status(500).json({ "error":"Unable to process the request" });
+  }
+
+
+  //Setting up the json response
   const GettLabels = (response) => {
   // Your code ends here //
   res.json({
